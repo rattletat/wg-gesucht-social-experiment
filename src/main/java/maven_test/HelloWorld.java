aec54803c50b6790e1cca0b6f3ea3f5791548001
Michael Brauweiler <michael.brauweiler@gmail.com>
2017-11-15 15:59:18 +0100

    Added 2 classes and 1 test from maven tutorial. Should work with EDI. maven_test can be deleted afterwards.

--- src/main/java/maven_test/HelloWorld.java
+++ src/main/java/maven_test/HelloWorld.java
@@ -2,6 +2,7 @@ package hello;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Greeter greeter = new Greeter();
+        System.out.println(greeter.sayHello());
     }
 }
