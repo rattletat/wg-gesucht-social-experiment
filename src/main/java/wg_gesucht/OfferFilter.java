8f1b938a263acb41cc01b4a8cfc5c565e17c42a3
Vadim Goryainov <vg.uni@t-online.de>
2017-11-30 23:43:26 +0100

    Offer Filter (gender, age)

---
+++ src/main/java/wg_gesucht/OfferFilter.java
@@ -0,0 +1,109 @@
+package wg_gesucht;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.LinkedList;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+public class OfferFilter {
+	
+	LinkedList<Document> filteredDocs;
+	boolean male;
+	boolean female;
+	int minAge;
+	int maxAge;
+	
+	public OfferFilter (boolean male, boolean female, int minAge, int maxAge)
+	{
+		filteredDocs = new LinkedList<Document>();
+		
+		this.male = male;
+		this.female = female;
+		this.minAge = minAge;
+		this.maxAge = maxAge;
+		
+		
+		File folderContacts = new File(OfferCrawler.filePathContacts);
+		File[] listOfFiles = folderContacts.listFiles();
+		for (File f : listOfFiles)
+		{
+			if (f.isFile() && f.getName().contains(".html"))
+			{
+				Document doc;
+				try {
+					doc = Jsoup.parse(f, "UTF-8", "");
+					if (checkDoc(doc)) filteredDocs.add(doc);
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+		
+	}
+	
+	public boolean checkDoc(Document doc)
+	{
+		//check availability
+		if (!doc.getElementsContainingOwnText("Kontaktaufnahme zur Zeit nicht möglich").isEmpty()) {
+			System.out.println("unavailable: "+doc.title() );
+			return false;
+		}
+		
+		//check gender
+		Element headlGesucht = doc.getElementsContainingOwnText("Gesucht wird").first();
+		if (headlGesucht == null) return true;
+		Element div = headlGesucht.parent();
+		String queryString;
+		if (male && female) queryString = "Frau|Mann";
+		else if (male && !female) queryString = "Mann";
+		else if (!male && female) queryString = "Frau";
+		else queryString = "impossible";
+		Elements els = div.select("*:matchesOwn("+queryString+")");
+		if (els.isEmpty()) {
+			System.out.println("no matching gender found: "+doc.title());
+			return false;
+		}
+		Element genderAgeInfo = els.first();
+		String genderAgeInfoString = genderAgeInfo.text();
+		
+		//check age
+		if (!genderAgeInfoString.contains("zwischen")) return true;
+		
+		String[] splittedInfo = genderAgeInfoString.split(" ");
+		String minAgeString = splittedInfo[splittedInfo.length - 4];
+		String maxAgeString = splittedInfo[splittedInfo.length - 2];
+		int minAgeParsed;
+		int maxAgeParsed;
+		try
+		{
+			minAgeParsed = Integer.parseInt(minAgeString);
+			maxAgeParsed = Integer.parseInt(maxAgeString);
+			if (minAgeParsed < minAge ||  maxAgeParsed > maxAge) {
+				System.out.println("not in age boundaries: "+doc.title());
+				return false;
+			}
+			
+		}catch (NumberFormatException e)
+		{
+			System.err.println("Could not read age boundaries in Document: "+ doc.title());
+			return false;
+		}
+		
+		
+		return true;
+	}
+	
+	
+	public void printFilteredDocs()
+	{
+		for (Document d: filteredDocs)
+		{
+			System.out.println(d.title());
+		}
+	}
+
+}
--- src/main/java/wg_gesucht/Starter.java
+++ src/main/java/wg_gesucht/Starter.java
@@ -5,7 +5,8 @@ import java.io.IOException;
 public class Starter {
 	
 	final static boolean startCityCrawler = false;
-	final static boolean startOfferCrawler = true;
+	final static boolean startOfferCrawler = false;
+	final static boolean startOfferFilter = true;
 
 	public static void main(String[] args) throws IOException {
 
@@ -19,6 +20,11 @@ public class Starter {
 		if (startOfferCrawler) 
 		{
 			new OfferCrawler();
+		}
+		
+		if (startOfferFilter) {
+			OfferFilter of = new OfferFilter(true, false, 20, 30);
+			of.printFilteredDocs();
 		}
 	}
 
