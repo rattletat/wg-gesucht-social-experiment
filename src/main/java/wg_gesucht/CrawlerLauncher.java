ec6a3b239c54e3b687da5ddf2e0e6395a5f50fba
Vadim Goryainov <vg.uni@t-online.de>
2017-12-28 19:26:31 +0100

    load infos from persona for filter

--- src/main/java/wg_gesucht/CrawlerLauncher.java
+++ src/main/java/wg_gesucht/CrawlerLauncher.java
@@ -1,6 +1,8 @@
 package wg_gesucht;
 
+import java.io.FileReader;
 import java.io.IOException;
+import java.util.Properties;
 
 public class CrawlerLauncher {
 
@@ -10,6 +12,25 @@ public class CrawlerLauncher {
     final static boolean startMessageWriter = true;
 
     public static void main(String[] args) throws IOException, InterruptedException {
+    	
+    	//load personas
+    	/*
+    	Properties[] personas = new Properties[2];
+    	FileReader reader = new FileReader(MessageWriter.filePathPersona1);
+		personas[0] = new Properties();
+		personas[0].load(reader);
+		
+		reader = new FileReader(MessageWriter.filePathPersona2);
+		personas[1] = new Properties();
+		personas[1].load(reader);
+		*/
+    	
+    	//load persona
+    	FileReader reader = new FileReader(MessageWriter.filePathPersona1);
+		Properties persona = new Properties();
+		persona.load(reader);
+    	
+		reader.close();
 
         if (startCityCrawler) {
             CityCrawler crawler = new CityCrawler();
@@ -22,7 +43,11 @@ public class CrawlerLauncher {
         }
 
         if (startOfferFilter) {
-            OfferFilter of = new OfferFilter(true, false, 25);
+        	boolean male;
+        	if (persona.getProperty("gender").equals("m")) male = true;
+        	else male = false;
+        	int age = Integer.parseInt(persona.getProperty("age"));
+            OfferFilter of = new OfferFilter(male, !male, age);
 
             if (startMessageWriter)
             {
